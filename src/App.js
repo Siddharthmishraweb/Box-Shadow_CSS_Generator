0cb97a76fddad44a9797dccaf54a6d866a7fb562
smishra-facilgo <smishra@anuktaitsolutions.com>
2022-06-11 22:15:02 +0530

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [Hori,setHori] = useState(-100);
+  const [Veri, setVeri] = useState(-100);
+  const [Blur,setBlur] = useState(10);
+  const [color,setColor] = useState('#363134');
+  const [checkOn,setcheckOn] = useState(false);
+  const [boxColor,setboxColor] = useState('#df0000');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='controls'> 
+        <label>Horizontal Length</label>        
+        <input type="range"  min="-200" max="200" value = {Hori} 
+          onChange = {(e)=>{
+            setHori(e.target.value);
+          }}
+        />         
+        <label>Vertical Length</label>
+        <input type="range"  min="-200" max="200" value = {Veri} 
+          onChange = {(e)=>{
+            setVeri(e.target.value);
+          }}
+        /> 
+        <label>Blur</label>
+        <input type="range"  min="0" max="200" value = {Blur} 
+          onChange = {(e)=>{
+            setBlur(e.target.value);
+          }}
+        />
+
+        <label>Color</label>
+        <br></br>
+        <input type="color" id="favcolor" name="favcolor" value={boxColor} 
+        onChange = {(e)=>{
+          setColor(e.target.value);
+        }}
+        /><br></br>
+        {/* <textarea type="color"   value = {color}  className = 'box-input'
+          onChange = {(e)=>{
+            setColor(e.target.value);
+          }}
+        />  */}
+        <label>Box Color</label>
+        <br></br>
+        <input type="color" id="favcolor" name="favcolor" value={color} 
+        onChange = {(e)=>{
+          setboxColor(e.target.value);
+        }}
+        /><br></br>
+        
+
+<div class="switch">
+    <label>
+      Outline
+      <input type="checkbox" checked ={checkOn} onChange={()=>setcheckOn(!checkOn)}/>
+      <span class="lever"></span>
+      Inset
+    </label>
+  </div>
+
+      </div>
+      <div className='output'>
+        <div className='box'style={{boxShadow:`${Hori}px ${Veri}px ${Blur}px ${color} ${checkOn?"inset":""}` , backgroundColor:`${boxColor}`}}>
+          box-shadow :     {Hori}px {Veri}px {Blur}px {color} {checkOn?"inset":""}
+        </div>
+      </div>
     </div>
   );
 }
